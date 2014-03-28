86c59fabcc09490bbc2f594e6c03ee886a1026df
Vincent Tinelli <vincent.tinelli@intel.com>
2014-03-28 15:39:59 +0100

    edison: enable mmc environment
    Signed-off-by: Vincent Tinelli <vincent.tinelli@intel.com>

--- build.sh
+++ build.sh
@@ -2,15 +2,28 @@
 
 rm u-boot.*
 BASE_DIR="/home/vincent/ndg/bootloader/ndg_edison-u-boot"
-TOOLS_DIR="/home/vincent/ndg/bootloader/ndg_edison-u-boot/tools/xfstk-stitcher/"
+TOOLS_DIR="/home/vincent/ndg/bootloader/ndg_edison-u-boot/tools"
+XFSTK_DIR="${TOOLS_DIR}/xfstk-stitcher/"
 
 make clean ; make edison_config; make -j
 
 BASE_IMAGE="${BASE_DIR}/u-boot.bin"
+ENV_FILE="${BASE_DIR}/arch/x86/cpu/tangier/default.env"
+ENV0_IMAGE="${BASE_DIR}/env0.bin"
+ENV1_IMAGE="${BASE_DIR}/env1.bin"
 NO_OSIP_IMAGE="${BASE_IMAGE}.no-osip"
+NO_OSIP_IMAGE_TMP="${BASE_IMAGE}.no-osip-tmp"
 FINAL_IMAGE="${BASE_IMAGE}.osip"
 
-cat u-boot.bin | dd of=${NO_OSIP_IMAGE} bs=4096 seek=1
+${TOOLS_DIR}/mkenvimage -s 0x10000 ${ENV_FILE} -o ${ENV0_IMAGE}
+${TOOLS_DIR}/mkenvimage -s 0x10000 ${ENV_FILE} -r -o ${ENV1_IMAGE}
 
+# FIXME: u-boot backup needs to have its own OSIP header
+cat u-boot.bin | dd of=${NO_OSIP_IMAGE_TMP} bs=4096 seek=1
+dd if=/dev/zero of=${NO_OSIP_IMAGE} bs=8M count=1
+dd if=${NO_OSIP_IMAGE_TMP} of=${NO_OSIP_IMAGE} bs=1M conv=notrunc seek=0
+dd if=${ENV0_IMAGE} of=${NO_OSIP_IMAGE} bs=1M conv=notrunc seek=2
+dd if=${NO_OSIP_IMAGE_TMP} of=${NO_OSIP_IMAGE} bs=1M conv=notrunc seek=2
+dd if=${ENV1_IMAGE} of=${NO_OSIP_IMAGE} bs=1M conv=notrunc seek=6
 
-${TOOLS_DIR}/gen_os --input ${NO_OSIP_IMAGE} --output ${FINAL_IMAGE} --xml ${TOOLS_DIR}/saltbay.XML
+${XFSTK_DIR}/gen_os --input ${NO_OSIP_IMAGE} --output ${FINAL_IMAGE} --xml ${TOOLS_DIR}/saltbay.XML
