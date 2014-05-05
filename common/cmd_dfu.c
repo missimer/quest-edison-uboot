15f74e7d2b69a12c9017f3741135cf0e8a1432ed
Vincent Tinelli <vincent.tinelli@intel.com>
2014-05-05 18:32:37 +0200

    Revert "[REVERTME] usb: 4 enumeration instead of 3"
    This reverts commit b80bd8e54a31762fc30c6bf3549423a8621ca981.
    
    Change-Id: If5e8b13fa31806e91eb1b640dcf9f2b94234fb5b
    Signed-off-by: Vincent Tinelli <vincent.tinelli@intel.com>
    Reviewed-on: http://git-ger-5.devtools.intel.com/gerrit/4747
    Tested-by: Chereau, Fabien <fabien.chereau@intel.com>
    Reviewed-by: Chereau, Fabien <fabien.chereau@intel.com>

--- common/cmd_dfu.c
+++ common/cmd_dfu.c
@@ -56,9 +56,9 @@ static int do_dfu(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		usb_gadget_handle_interrupts();
 		workaround++;
 
-		 if (workaround < 4)
+		 if (workaround < 3)
 		 {
-		     mdelay(500);
+		     mdelay(2000);
 		     g_dnl_unregister();
 		     dfu_free_entities();
 		     do_dfu(cmdtp, flag, argc, argv);
